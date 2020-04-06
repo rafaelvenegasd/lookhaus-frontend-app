6cd3b1a982f8e4e663014b438212658d04cb47a4
Rafael <arvenegasd@gmail.com>
2020-04-06 15:33:23 +0200

    structure of the folders [update]

--- device-detector.js
+++ device-detector.js
@@ -2,8 +2,8 @@
 require('http').createServer(function(req, res){
       DeviceDetector = require("device-detector-js");
       const deviceDetector = new DeviceDetector();
-      const userAgent = request.headers['user-agent'];
+      const userAgent = req.headers['user-agent'];
       const detector = deviceDetector.parse(userAgent);
       const device = detector.device.type; 
-      module.exports(device);
-}).listen(8080);
+      console.log(device);
+}).listen(process.env.PORT || 3000);
---
+++ src/mobile/views/main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <div class="d-flex mt-2 mb-2 container">
+        <Content></Content>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/navbar.vue'
+import Content from '../components/content.vue'
+import Footer from '../components/footer.vue'
+
+export default {
+  name: "App",
+  components:{
+    Navbar,
+    Content, 
+    Footer
+  }
+}
+</script>
