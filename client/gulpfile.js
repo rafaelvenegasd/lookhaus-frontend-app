6f63fa8f7861c7e709262060351879bb809c69ab
Rafael <arvenegasd@gmail.com>
2020-04-09 21:37:24 +0200

    gulp [fixing]

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -7,18 +7,18 @@ const notify = require( 'gulp-notify' );
 function desktop() {
     return (
       gulp
-        .src("./assets/styles/web/importer.scss")
+        .src("./src/assets/styles/web/importer.scss")
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest("./assets/styles/web/"))
+        .pipe(gulp.dest("./src/assets/styles/web/"))
         .pipe( notify({ message: '\n\n✅  ===> DESKTOP STYLE — completed!\n', onLast: true }))
     );
 }
 function mobile() {
     return (
       gulp
-        .src("./assets/styles/mobile/importer.scss")
+        .src("./src/assets/styles/mobile/importer.scss")
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest("./assets/styles/mobile/"))
+        .pipe(gulp.dest("./src/assets/styles/mobile/"))
         .pipe( notify({ message: '\n\n✅  ===> MOBILE STYLE — completed!\n', onLast: true }))
     );
 }
