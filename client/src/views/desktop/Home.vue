e590edcb2390a7a1c6281a2ccf4a31b92e0224bf
Rafael <arvenegasd@gmail.com>
2020-04-14 01:47:16 +0200

    home design [update]

--- client/src/views/desktop/Home.vue
+++ client/src/views/desktop/Home.vue
@@ -1,19 +1,26 @@
 <template>
-  <div>
+  <!-- <div class="img-searcher-container"> -->
     <div class="img-searcher-container d-flex justify-content-center">
       <div class="d-flex justify-content-center align-items-center flex-column container">
         <div class="searcher-container">
-          <h1 class="text-white">Lookhaus</h1>
-           <h2 class="text-white">Some text will be here</h2>
-            <div id="searcher"><div class="inputWithIcon">
-              <input type="text" placeholder="Search markets by name">
-               <i aria-hidden="true" class="material-icons">search</i>
-              </div>
+          <div class="d-flex justify-content-center align-items-center flex-column container mb-3">
+            <img alt="Lookhaus logo" src="../../assets/images/logos/black-small.png" class="logo mb-2 pt-3"> 
+            <span>Find the perfect place</span>
+          </div>
+          <div class="d-flex justify-content-center align-items-center flex-column container">
+            <div>
+              <input type="radio" name="place" id="live" class="mr-2 btn-radio"><label for="place" class="mr-3">Place to live</label>
+              <input type="radio" name="place" id="work" class="mr-2 btn-radio"><label for="place">Place to work / Office</label>
+            </div>
+          </div>
+            <div id="searcher" class="d-flex justify-content-center align-items-center flex-column container mb-4">
+                <input type="text" placeholder="Type city, town, etc." class="col-form-label-sm">
+                <input type="submit" value="Search" class="btn">
             </div>
           </div>
         </div>
       </div> 
-    </div>
+    <!-- </div> -->
 </template>
 
 <script>
