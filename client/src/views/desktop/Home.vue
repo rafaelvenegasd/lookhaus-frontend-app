cdbb6f30c7ac053cf3432d689cb4653f72a8a64e
Rafael <arvenegasd@gmail.com>
2020-04-13 12:39:46 +0200

    design [update]

--- client/src/views/desktop/Home.vue
+++ client/src/views/desktop/Home.vue
@@ -3,8 +3,8 @@
     <div class="img-searcher-container d-flex justify-content-center">
       <div class="d-flex justify-content-center align-items-center flex-column container">
         <div class="searcher-container">
-          <h1 class="text-white">The World’s Leading Exchange Platform</h1>
-           <h2 class="text-white">Trade Bitcoin, BNB, and hundreds of other cryptocurrencies in minutes</h2>
+          <h1 class="text-white">Lookhaus</h1>
+           <h2 class="text-white">Some text will be here</h2>
             <div id="searcher"><div class="inputWithIcon">
               <input type="text" placeholder="Search markets by name">
                <i aria-hidden="true" class="material-icons">search</i>
