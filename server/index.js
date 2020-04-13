7a064276ad29e37489fb5d47ca2985c07e0fbc75
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-04-13 18:40:38 +0200

    heroku

--- server/index.js
+++ server/index.js
@@ -20,5 +20,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Connect server
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
