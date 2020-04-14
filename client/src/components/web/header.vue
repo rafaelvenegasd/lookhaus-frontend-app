1b1d03b3e15dd35b6c8659a794631f6e5c86358a
Rafael <arvenegasd@gmail.com>
2020-04-14 14:55:03 +0200

    home view [design update]

--- client/src/components/web/header.vue
+++ client/src/components/web/header.vue
@@ -2,9 +2,9 @@
 <header>
   <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
     <a href="/" class="navbar-brand v-market-logo router-link-active"><img alt="Lookhaus logo" src="../../assets/images/logos/white-small.png" class="logo"></a> 
-      <ul class="row">
+      <ul class="row navbar">
         <li><a href="#" class="nav-link"><input type="submit" class="btn" value="Post your Place"></a></li> 
-        <li><a href="#" class="nav-link"><button class="btn transparent">Login</button></a></li>
+        <li><a href="#" class="nav-link"><button class="btn transparent label">Login</button></a></li>
       </ul>
   </nav>
 </header>
