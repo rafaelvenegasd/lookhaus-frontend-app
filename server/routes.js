203689aeec49347dd8ccfc0b9259cc904f4ccdaa
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-04-15 17:15:22 +0200

    fixing template string, it didin't work on last commit

--- server/routes.js
+++ server/routes.js
@@ -9,7 +9,14 @@ router.get(/.*/, (req, res) => {
 	const userAgent = req.headers["user-agent"];
 	const detector = deviceDetector.parse(userAgent);
 	const device = detector.device.type;
-	res.sendFile(`${__dirname} + '/public/${device}/index.html`);
+
+	res.sendFile(__dirname + `/public/${device}/index.html`);
+
+	// if (device == 'desktop'){
+	// 	res.sendFile(__dirname + '/public/desktop/index.html');
+	// }else{
+	// 	res.sendFile(__dirname + '/public/mobile/index.html');
+	// }
 });
 
 module.exports = router;
